c8c9ca174f90d5836a32a7e4c7edfee95a622deb
jennifer <jennifersantossouza@hotmail.com>
2025-01-06 16:27:24 -0300

    feat: converter implemented

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from tkinter import *
 
-def calculate():
-    pass
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    result_label.config(text=f"{km}")
 
 window = Tk()
 window.title("Mile to Km")
 window.config(padx=20, pady=20)
 
 
-miles_input = Entry()
+miles_input = Entry(width=5)
 miles_input.grid(column=1, row=0)
 
 miles_label = Label(text="Miles")
@@ -23,7 +25,7 @@ result_label.grid(column=1, row=1)
 km_label = Label(text="Km")
 km_label.grid(column=2, row=1)
 
-result_button = Button(text="Calculate", command=calculate)
-
+result_button = Button(text="Calculate", command=miles_to_km)
+result_button.grid(column=1, row=2)
 
 window.mainloop()
